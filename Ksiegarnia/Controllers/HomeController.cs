df61ea43278054519c4c79e569eaa245580778e1
Błażej Tempski <blazej33.9@gmail.com>
2020-01-23 22:43:05 +0100

    Add email sending

--- Ksiegarnia/Controllers/HomeController.cs
+++ Ksiegarnia/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Ksiegarnia.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace Ksiegarnia.Controllers
 {
@@ -33,6 +35,30 @@ namespace Ksiegarnia.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult SendEmail(string subject, string body, string name, string email)
+        {
+            MailMessage message = new MailMessage();
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = false;
+            message.From = new MailAddress("ksiegarniazajeciowa@gmail.com", name);
+            message.To.Add("ksiegarniazajeciowa@gmail.com");
+            message.ReplyToList.Add(email);
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+
+            NetworkCredential networkCredential = new NetworkCredential("ksiegarniazajeciowa@gmail.com", "Qwerty123.");
+            smtp.UseDefaultCredentials = true;
+            smtp.Credentials = networkCredential;
+
+            smtp.Send(message);
+
+            return View("Contact");
         }
     }
 }
